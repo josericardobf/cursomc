026615306320c8b2cdec8f42bf9af217df2e8857
josericardobf <josericardobf@gmail.com>
2018-09-06 22:28:45 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/josericardo/cursomc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.josericardo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.josericardo.cursomc.domain.Cliente;
+import com.josericardo.cursomc.repositories.ClienteRepository;
+import com.josericardo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
