f2fa43327a4a188ae9eb90f4bbda67ef4f1a9177
josericardobf <josericardobf@gmail.com>
2018-09-06 22:13:31 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/josericardo/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.josericardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.josericardo.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
