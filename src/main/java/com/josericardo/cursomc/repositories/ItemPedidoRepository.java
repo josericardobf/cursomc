750b8f4af2aad320bb117caafe7b628269413999
josericardobf <josericardobf@gmail.com>
2018-09-13 20:30:19 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/josericardo/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.josericardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.josericardo.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
