2a994517650850775f82a5c3a38515dfc540657f
josericardobf <josericardobf@gmail.com>
2018-09-13 21:02:35 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/josericardo/cursomc/services/PedidoService.java
@@ -0,0 +1,29 @@
+package com.josericardo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.josericardo.cursomc.domain.Pedido;
+import com.josericardo.cursomc.repositories.PedidoRepository;
+import com.josericardo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
