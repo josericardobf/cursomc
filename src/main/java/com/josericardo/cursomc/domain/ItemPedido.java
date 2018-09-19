e42786e358efcc8498699028da936b220023859f
josericardobf <josericardobf@gmail.com>
2018-09-18 22:08:59 -0300

    Listando todas categorias com DTO

--- src/main/java/com/josericardo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/josericardo/cursomc/domain/ItemPedido.java
@@ -2,7 +2,6 @@ package com.josericardo.cursomc.domain;
 
 import java.io.Serializable;
 
-import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
